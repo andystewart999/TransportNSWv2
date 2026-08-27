578a083950a8a40d07b7e3cfa355ed88807799c1
Andy Stewart <andy.stewart@live.com>
2026-08-27 10:09:06 +1000

    Bump version to 3.3.2

--- TransportNSWv2/__init__.py
+++ TransportNSWv2/__init__.py
@@ -1,4 +1,4 @@
 """Define module-level imports."""
 from TransportNSWv2.TransportNSWv2 import TransportNSWv2, InvalidAPIKey, APIRateLimitExceeded, StopError, TripError
 
-__version__ = "3.2.0"
+__version__ = "3.3.2"
